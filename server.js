ab5ec98f92a24d91603f3469d9df7a7ea4421e00
apexweb66-hub <apexweb66@gmail.com>
2026-04-07 23:19:58 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -13,35 +13,67 @@ app.post("/api/calls/incoming", async (req, res) => {
   const from = req.body.From || "Unknown";
 
   try {
-    const response = await axios.get(
-  `http://apilayer.net/api/validate?access_key=f74827e20dae4f557a2de6b0e55caa63&number=${from}`
-);
+  const response = await axios.get(
+    `http://apilayer.net/api/validate?access_key=f74827e20dae4f557a2de6b0e55caa63&number=${from}`
+  );
 
-    const data = response.data;
+  const data = response.data;
 
-    if (!data.valid) {
-      return res.send(`
-        <Response>
-          <Say>This call has been blocked.</Say>
-          <Hangup/>
-        </Response>
-      `);
-    }
-
+  if (!data.valid) {
     return res.send(`
       <Response>
-        <Say>Connecting your call</Say>
-        <Dial>+19018206993</Dial>
-      </Response>
-    `);
-  } catch (err) {
-    console.error(err);
-
-    return res.send(`
-      <Response>
-        <Say>Error occurred. Connecting call.</Say>
-        <Dial>+19018206993</Dial>
+        <Say>This call has been blocked.</Say>
+        <Hangup/>
       </Response>
     `);
   }
-});
+
+  return res.send(`
+    <Response>
+      <Say>Connecting your call</Say>
+      <Dial>+19018206993</Dial>
+    </Response>
+  `);
+
+} catch (err) {
+  console.error(err);
+
+  return res.send(`
+    <Response>
+      <Say>Error occurred. Connecting call.</Say>
+      <Dial>+19018206993</Dial>
+    </Response>
+  `);
+try {
+  const response = await axios.get(
+    `http://apilayer.net/api/validate?access_key=f74827e20dae4f557a2de6b0e55caa63&number=${from}`
+  );
+
+  const data = response.data;
+
+  if (!data.valid) {
+    return res.send(`
+      <Response>
+        <Say>This call has been blocked.</Say>
+        <Hangup/>
+      </Response>
+    `);
+  }
+
+  return res.send(`
+    <Response>
+      <Say>Connecting your call</Say>
+      <Dial>+19018206993</Dial>
+    </Response>
+  `);
+
+} catch (err) {
+  console.error(err);
+
+  return res.send(`
+    <Response>
+      <Say>Error occurred. Connecting call.</Say>
+      <Dial>+19018206993</Dial>
+    </Response>
+  `);
+}
