ca9b24481b4d7c3e2ab4f42f1b1927a060f7ec1f
apexweb66-hub <apexweb66@gmail.com>
2026-04-08 00:23:49 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -13,38 +13,39 @@ app.post("/api/calls/incoming", async (req, res) => {
   const from = req.body.From || "Unknown";
 
   try {
-  const response = await axios.get(
-    `http://apilayer.net/api/validate?access_key=f74827e20dae4f557a2de6b0e55caa63&number=${from}`
-  );
+    const response = await axios.get(
+      `http://apilayer.net/api/validate?access_key=f74827e20dae4f557a2de6b0e55caa63&number=${from}`
+    );
 
-  const data = response.data;
+    const data = response.data;
 
-  if (!data.valid) {
+    if (!data.valid) {
+      return res.send(`
+        <Response>
+          <Say>This call has been blocked.</Say>
+          <Hangup/>
+        </Response>
+      `);
+    }
+
     return res.send(`
       <Response>
-        <Say>This call has been blocked.</Say>
-        <Hangup/>
+        <Say>Connecting your call</Say>
+        <Dial>+19018206993</Dial>
+      </Response>
+    `);
+
+  } catch (err) {
+    console.error(err);
+
+    return res.send(`
+      <Response>
+        <Say>Error occurred. Connecting call.</Say>
+        <Dial>+19018206993</Dial>
       </Response>
     `);
   }
-
-  return res.send(`
-    <Response>
-      <Say>Connecting your call</Say>
-      <Dial>+19018206993</Dial>
-    </Response>
-  `);
-
-} catch (err) {
-  console.error(err);
-
-  return res.send(`
-    <Response>
-      <Say>Error occurred. Connecting call.</Say>
-      <Dial>+19018206993</Dial>
-    </Response>
-  `);
-}
+});
 
 const PORT = process.env.PORT || 3000;
 
